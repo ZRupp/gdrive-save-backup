19e6bdbacc01f88852a13c86e398f0fe8693de7d
ZRupp <zachariarupp@gmail.com>
2023-09-07 19:22:37 -0700

    Use pydrive2 and fsspec for easy file manipulation.

--- g_drive.py
+++ g_drive.py
@@ -1,30 +1,31 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from pydrive2.fs import GDriveFileSystem
 
 class GDrive:
     '''A Class allowing manipulation of the GoogleDrive object'''
     def __init__(self):
-        self.gauth = GoogleAuth()
-        self.gauth.LocalWebserverAuth()
-        self.drive = GoogleDrive(self.gauth)
+        self.__gauth = GoogleAuth()
+        self.__gauth.LocalWebserverAuth()
+        self.__drive = GoogleDrive(self.__gauth)
+        self.__fs = GDriveFileSystem(
+            "root",
+            google_auth=self.__gauth
+        )
 
     
     def get_file_list(self, query) -> list:
         '''Returns a list of folders and/or files matching the query string'''
 
-        return self.drive.ListFile({'q': "title = 'saves' and trashed=false and mimeType = 'application/vnd.google-apps.folder'"}).GetList()
-
-
-if __name__ == '__main__':
+        return self.__drive.ListFile(query).GetList()
     
+    def upload_to_g_drive(self, local_path: str, remote_path: str = 'root/saves/') -> None:
+        ''' Simple method to upload file to GDrive.
+        '''
 
-    g_drive = GDrive()
+        self.__fs.put(local_path, remote_path, recursive=True)
 
-    query = {'q': "name contains 'saves' and trashed=false"}
+    def file_exists(self, path: str) -> bool:
+        '''Simple method to check if the file exists remotely'''
+        return self.__fs.exists(path)
 
-    file_list = g_drive.get_file_list(query)
-
-    for f in file_list:
-        print(f)
-
-    
