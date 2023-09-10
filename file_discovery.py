bafa2f2653313b52df2820ab9286cad545ce5ebc
ZRupp <zachariarupp@gmail.com>
2023-09-10 11:11:59 -0700

    Remove hard-coded paths. Refactor for style.

--- file_discovery.py
+++ file_discovery.py
@@ -1,44 +1,48 @@
-
 import os
 import re
 import json
 import g_drive
 
 # Don't really like this, but we'll figure out a better solution later
-STEAM_PATH = ['C:/Program Files (x86)/Steam/steamapps/common']
-PC_DEFAULT = ['C:/Users']
+# Maybe save these to a configuration file in json?
+STEAM_PATH = ["C:/Program Files (x86)/Steam/steamapps/common"]
+PC_DEFAULT = ["C:/Users"]
+
+REMOTE_SAVE_PATH = "root/saves/"
+DISCOVERED_FOLDERS_PATH = "./data/discovered_folders.json"
 
 
 def save_discovered_folders_to_json(discovered_folders: dict) -> int:
-    '''Simple method for saving discovered folders in json format.
+    """Simple method for saving discovered folders in json format.
 
     returns int for success status
 
     TODO: - Error Handling
-    '''
+    """
 
-    with open('./data/discovered_folders.json', 'w', encoding='utf-8') as f:
+    with open(DISCOVERED_FOLDERS_PATH, "w", encoding="utf-8") as f:
         json.dump(discovered_folders, f, ensure_ascii=False)
 
     return 1
 
 
 def load_discovered_folders_from_json(path: str) -> dict:
-    '''Simple method for a json file populated with games and the save folder locations.
+    """Simple method for a json file populated with games and the save folder locations.
 
     returns a dict in the form {'game_name': 'path_to_game'}
 
     TODO:   - Either in this method or another, ensure that folders exist and remove
               them if not.
-            - Error Handling  
-    '''
-    with open(path, 'r', encoding='utf-8') as f:
+            - Error Handling
+    """
+    with open(path, "r", encoding="utf-8") as f:
         discovered_folders = json.load(f)
 
     return discovered_folders
 
+
 def discover_folders(path: str) -> int:
-    '''Method to discover location of save files. Currently only seeks steam save data in Windows.
+    """Method to discover location of save files. Currently only seeks steam save data in Windows.
 
     returns success state as int
 
@@ -50,26 +54,27 @@ def discover_folders(path: str) -> int:
                 * Others?
             - Discover folders in AppData and other common locations
             - Error Handling
-    '''
+    """
 
-    discovered_folders = load_discovered_folders_from_json('./data/discovered_folders.json') # Hard-coded value bad
-    
-    
+    discovered_folders = load_discovered_folders_from_json(DISCOVERED_FOLDERS_PATH)
+
+    # This is specific to Steam games on Windows. Edit for other launchers.
+    re_query = "(?<=common[\\\]).*(?=\\\)|(?<=common[\\\]).*"
+
     for root, dirs, files in os.walk(path):
-        matching = [dir for dir in dirs if re.search(r'^save|^Save', dir)]
+        matching = [dir for dir in dirs if re.search(r"^save|^Save", dir)]
         if matching:
-            game_name = re.search("(?<=common[\\\]).*(?=\\\)|(?<=common[\\\]).*", root)[0]
+            game_name = re.search(re_query, root)[0]
             if game_name not in discovered_folders:
                 for match in matching:
-                    
                     discovered_folders[game_name] = os.path.join(root, match)
 
     save_discovered_folders_to_json(discovered_folders)
-    
+
     return 1
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
     paths = STEAM_PATH
 
     for path in paths:
@@ -77,10 +82,9 @@ if __name__ == '__main__':
 
     drive = g_drive.GDrive()
 
-    discovered_folders = load_discovered_folders_from_json('./data/discovered_folders.json')
+    discovered_folders = load_discovered_folders_from_json(DISCOVERED_FOLDERS_PATH)
 
     print(discovered_folders)
 
     for game_name, folder in discovered_folders.items():
-        drive.upload_to_g_drive(folder +  '/*', f'root/saves/{game_name}/')
-            
+        drive.upload_to_g_drive(folder + "/*", f"{REMOTE_SAVE_PATH}{game_name}/")
--- g_drive.py
+++ g_drive.py
@@ -1,24 +1,29 @@
 from pydrive2.auth import GoogleAuth
-#from pydrive2.drive import GoogleDrive
+
+# from pydrive2.drive import GoogleDrive
 from pydrive2.fs import GDriveFileSystem
 
+
 class GDrive:
-    '''A Class allowing manipulation of the GoogleDrive object'''
+    """A Class allowing manipulation of the GoogleDrive object"""
+
     def __init__(self):
         self.__gauth = GoogleAuth()
         self.__gauth.LocalWebserverAuth()
-        self.__fs = GDriveFileSystem(
-            "root",
-            google_auth=self.__gauth
-        )
-    
-    def upload_to_g_drive(self, local_path: str, remote_path: str = 'root/saves/') -> None:
-        ''' Simple method to upload file to GDrive.
-        '''
+        self.__fs = GDriveFileSystem("root", google_auth=self.__gauth)
+
+    def upload_to_g_drive(
+        self,
+        local_path: str,
+        remote_path: str = "root/saves/",
+    ) -> None:
+        """Simple method to upload file to GDrive.
+
+        TODO: Only write to gdrive if file doesn't exist.
+        """
 
         self.__fs.put(local_path, remote_path, recursive=True)
 
     def file_exists(self, path: str) -> bool:
-        '''Simple method to check if the file exists remotely'''
+        """Simple method to check if the file exists remotely"""
         return self.__fs.exists(path)
-
